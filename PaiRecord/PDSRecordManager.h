eb2a92ae78bf49cb921df7b68a77e9e85e5e9e93
lightning <songjianw@gmail.com>
2015-05-27 19:53:30 +0800

    add camera api

---
+++ PaiRecord/PDSRecordManager.h
@@ -0,0 +1,51 @@
+//
+//  PDSRecordManager.h
+//  paidashi
+//
+//  Created by Lightning on 15/5/27.
+//  Copyright (c) 2015年 Lightning. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+@protocol PDSRecordManagerDelegate <NSObject>
+
+- (void)recordingWillStart;
+- (void)recordingDidStart;
+- (void)recordingWillStop;
+- (void)recordingDidStop;
+
+- (void)totalRecordingTime:(NSInteger)timeCount formaterString:(NSString *)timeString;
+
+@end
+
+
+@interface PDSRecordManager : NSObject
+
+#warning single to do
+
+@property (nonatomic, assign) BOOL enableMicro;
+@property (nonatomic, assign) BOOL enableFlash;
+@property (nonatomic, assign, getter=isRecording) BOOL recording;
+@property (nonatomic, weak) id<PDSRecordManagerDelegate> delegate;
+
+@property (nonatomic, strong, readonly) AVCaptureSession *captureSession;
+@property (nonatomic, strong) NSURL *fileUrl;
+
+
+//@property (nonatomic, assign)
+
+- (void)switchCamera;
+- (void)takeSnap;
+
+- (void)startRecord;
+- (void)stopRecord;
+- (void)pauseRecord;
+- (void)resumeRecord;
+
+- (void)setupSessionContext;
+- (void)tearDownSessionContext;
+
+
+
+@end
---
+++ PaiRecord/PDSRecordManager.m
@@ -0,0 +1,13 @@
+//
+//  PDSRecordManager.m
+//  paidashi
+//
+//  Created by Lightning on 15/5/27.
+//  Copyright (c) 2015年 Lightning. All rights reserved.
+//
+
+#import "PDSRecordManager.h"
+
+@implementation PDSRecordManager
+
+@end
